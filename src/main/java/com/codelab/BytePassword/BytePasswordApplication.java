7c5d8ffc9230194079f705c0df352471cd2978e8
Rurangwa <chrisruu@yahoo.com>
2023-11-17 10:39:58 +0800

    initial commit

---
+++ src/main/java/com/codelab/BytePassword/BytePasswordApplication.java
@@ -0,0 +1,13 @@
+package com.codelab.BytePassword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BytePasswordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BytePasswordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codelab/BytePassword/BytePasswordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codelab.BytePassword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BytePasswordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
